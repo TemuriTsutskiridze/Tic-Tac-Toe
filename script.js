07e45a56be2c70d6c4d942883a1ad08aa912d2f4
Temuri Tsutskiridze <temuri.tsutskiridze.04@gmail.com>
2023-02-27 21:46:27 +0400

    quit, next round, reset

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 `use strict`
-// menu elements
+// -------------------------- menu elements ----------------------//
 
 let menu = document.querySelector(".menu");
 let choose_buttons = document.getElementsByClassName("choose_container");
@@ -8,16 +8,30 @@ let o = document.getElementById("o");
 let cpu = document.getElementById("CPU");
 let vs_player = document.getElementById("PLAYER");
 
-// game elements
+// ---------------------- game elements ---------------------- //
 
 let game = document.querySelector(".game");
 let restart = document.querySelector(".restart");
-// let boxes = document.getElementsByClassName("box");
+let boxes = document.querySelectorAll(".box");
 let turn = document.getElementById("turn");
 
-// menu functions, variables and eventlisteners
+// --------------------- win elements -------------------- //
+
+let win_background = document.querySelector(".win_background");
+let win_icon = document.querySelector(".winner_icon");
+let winner_text = document.querySelector(".winner_text");
+let player = document.querySelector(".player");
+let quit = document.querySelector(".quit");
+let next_round = document.querySelector(".next");
+
+// ---------- menu functions, variables and eventlisteners ------------ //
+
+game.style.display = "none";
+win_background.style.display = "none";
 
 let first_player;
+
+// adding and removing hover classes to game grid elements
 
 Array.from(choose_buttons).forEach(element => {
     element.addEventListener("click", () => {
@@ -33,98 +47,162 @@ Array.from(choose_buttons).forEach(element => {
     })
 });
 
-game.style.display = "none";
 
+// player vs player mode 
 vs_player.addEventListener("click", () => {
-    menu.style.display = "none";
-    game.style.display = "flex"
+    if (first_player != undefined) {
+        menu.style.display = "none";
+        game.style.display = "flex"
+    }
+});
+
+quit.addEventListener("click", () => {
+    win_background.style.display = "none";
+    reset();
+});
+
+next_round.addEventListener("click", () => {
+    win_background.style.display = "none";
+    reset();
 });
 
 
+// --------------------- game functions, variables and eventlisteners --------------------- //
 
-// game functions, variables and eventlisteners
 
-restart.addEventListener("click", () => {
-    game.style.display = "none"
-    menu.style.display = "flex";
-});
 
-player_turn = "x";
-
-let boxes = document.querySelectorAll(".box");
+let player_turn = "x";
 
 let clickedButtons = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
 ];
 
+reset();
 
-Array.from(boxes).forEach(element => {
-    element.dataset.row = Math.floor(element.value / 3);
-    element.dataset.col = element.value % 3;
-    element.addEventListener("click", handleClick);
+restart.addEventListener("click", () => {
+    reset();
+});
+
+quit.addEventListener("click", () => {
+    win_background.style.display = "none";
+    game.style.display = "none";
+    menu.style.display = "flex";
+    player_turn = "x";
+    reset();
 });
 
 
+
+// -------------------------- functions --------------------------- //
+
+function reset() {
+    Array.from(boxes).forEach((element, index) => {
+        element.dataset.row = Math.floor(index / 3);
+        element.dataset.col = index % 3;
+        element.dataset.playerTurn = "x"; // set default player turn
+        element.style.backgroundImage = "none";
+        element.classList.add("x_hover");
+        element.classList.remove("o_hover");
+        element.addEventListener("click", handleClick);
+    });
+    clickedButtons = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ];
+    player_turn = "x";
+}
+
+// functionality after click on game grid element
 function handleClick(event) {
     let element = event.target;
     let row = element.dataset.row;
     let col = element.dataset.col;
 
-    
     if (player_turn == "x") {
         element.style.backgroundImage = "url(./assets/icon-x.svg)";
         clickedButtons[row][col] = "x";
         player_turn = "o";
-        turn.firstElementChild.innerHTML = '<path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#F2B137"/>';
-        
-    } else {
+        turn.firstElementChild.innerHTML = '<path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="var(--gray-dark)"/>';
+        // removing and adding hover classes to boxes that have not been clicked
+        Array.from(boxes).forEach(box => {
+            for (let row = 0; row <= 2; row++) {
+                for (let col = 0; col <= 2; col++) {
+                    if (typeof clickedButtons[row][col] === 'number') {
+                        if (box.value == clickedButtons[row][col]) {
+                            box.classList.remove("x_hover");
+                            box.classList.add("o_hover");
+                        }
+                    }
+                }
+            }
+        });
+        element.classList.remove("x_hover");
+        element.classList.remove("o_hover");
+        checkForWin();
+    } else if (player_turn == "o") {
         element.style.backgroundImage = "url(./assets/icon-o.svg)";
         clickedButtons[row][col] = "o";
         player_turn = "x";
         turn.firstElementChild.innerHTML = '<path d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z" fill="var(--gray-dark)" fill-rule="evenodd"/>';
+        // removing and adding hover classes to boxes that have not been clicked
+        Array.from(boxes).forEach(box => {
+            for (let row = 0; row <= 2; row++) {
+                for (let col = 0; col <= 2; col++) {
+                    if (typeof clickedButtons[row][col] === 'number') {
+                        if (box.value == clickedButtons[row][col]) {
+                            box.classList.remove("o_hover");
+                            box.classList.add("x_hover");
+                        }
+                    }
+                }
+            }
+        });;
+        element.classList.remove("x_hover");
+        element.classList.remove("o_hover");
+        checkForWin();
     }
+
     // Remove the event listener from the clicked button
     element.removeEventListener("click", handleClick);
     console.log(clickedButtons);
-    
 }
 
-// let clickedButtons = [
-//     [0, 0, 0],
-//     [0, 0, 0],
-//     [0, 0, 0]
-// ];
-
-// Array.from(boxes).forEach(element => {
-//     element.addEventListener("click", () => {
-//         let row = Math.floor((element.value - 1) / 3);
-//         let col = (element.value - 1) % 3;
-
-//         if (player_turn == "x") {
-//             element.style.backgroundImage = "url(./assets/icon-x.svg)";
-//             player_turn = "o";
-//             turn.firstElementChild.innerHTML = '<path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#F2B137"/>';
-//             clickedButtons[row][col] = "x";
-//             console.log(clickedButtons);    
-//         } else {
-//             element.style.backgroundImage = "url(./assets/icon-o.svg)";
-//             player_turn = "x";
-//             turn.firstElementChild.innerHTML = '<path d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z" fill="var(--gray-dark)" fill-rule="evenodd"/>';            
-//             clickedButtons[row][col] = "o";
-//             console.log(clickedButtons);    
-//         }
-//     });
-    
-// });
-
-
-// for (let clickedButton of clickedButtons) {
-//     if (clickedButton != 0) {
-//         clickedButton
-//     }
-// }
+// check for win positions
+function checkForWin() {
+    if ((clickedButtons[0][0] === clickedButtons[0][1] && clickedButtons[0][1] === clickedButtons[0][2]) ||
+        (clickedButtons[1][0] === clickedButtons[1][1] && clickedButtons[1][1] === clickedButtons[1][2]) ||
+        (clickedButtons[2][0] === clickedButtons[2][1] && clickedButtons[2][1] === clickedButtons[2][2]) ||
+        (clickedButtons[0][0] === clickedButtons[1][0] && clickedButtons[1][0] === clickedButtons[2][0]) ||
+        (clickedButtons[0][1] === clickedButtons[1][1] && clickedButtons[1][1] === clickedButtons[2][1]) ||
+        (clickedButtons[0][2] === clickedButtons[1][2] && clickedButtons[2][2] === clickedButtons[2][2]) ||
+        (clickedButtons[0][0] === clickedButtons[1][1] && clickedButtons[1][1] === clickedButtons[2][2]) ||
+        (clickedButtons[0][2] === clickedButtons[1][1] && clickedButtons[1][1] === clickedButtons[2][0])) {
+            if (player_turn == "x") {
+                win_background.style.display = "flex";
+                win_icon.src = "assets/icon-o.svg";
+                winner_text.style.color = "var(--yellow-light)";
+                if (first_player == "x") {
+                    player.textContent = "PLAYER 1 WINS";
+                } else if (first_player = "o"){
+                    player.textContent = "PLAYER 2 WINS";
+                }
+                console.log("o");
+            } else {
+                win_background.style.display = "flex";
+                win_icon.src = "assets/icon-x.svg";
+                winner_text.style.color = "var(--blue-light)";
+                if (first_player == "x") {
+                    player.textContent = "PLAYER 1 WINS";
+                } else if (first_player = "o"){
+                    player.textContent = "PLAYER 2 WINS";
+                }
+                console.log("x");
+            }
+    }
+}
 
 
 
