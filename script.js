b50dcb1eeaf9a8b7db8ddde548cdbd739436ea9e
Temuri Tsutskiridze <temuri.tsutskiridze.04@gmail.com>
2023-03-07 14:51:06 +0400

    player turn svg fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 `use strict`
 // -------------------------- menu elements ----------------------//
-
+debugger
 let menu = document.querySelector(".menu");
 let choose_buttons = document.getElementsByClassName("choose_container");
 let x = document.getElementById("x");
@@ -147,6 +147,9 @@ function reset() {
         [7, 8, 9]
     ];
     player_turn = "x";
+
+    turn.firstElementChild.innerHTML = '<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z" fill="var(--gray-dark)" fill-rule="evenodd"/></svg>'
+
 }
 
 // functionality after click on game grid element
